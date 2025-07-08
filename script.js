627ce2526b236be8cd1847f4e5a823cec9e3cdaf
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-07-08 20:46:06 -0300

    Escribe la logica para tener la eleccion de la pc

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello World!");
+
+let getComputerChoice = () => {
+  let choice = Math.floor(Math.random() * 3) + 1;
+
+  switch (choice) {
+    case 1:
+      return "Rock";
+    case 2:
+      return "Paper";
+    case 3:
+      return "Scissors";
+  }
+};
+
+console.log(getComputerChoice());
